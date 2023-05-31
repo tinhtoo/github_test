c3cfcf6f0563733d631bbd1a0fdddbb234632f16
tinhtooaung <tinhtooaungyau@gmail.com>
2023-05-31 13:01:46 +0900

    Update email.php

--- email.php
+++ email.php
@@ -5,8 +5,4 @@ echo "email sending programm";
 
 echo "second email send";
 
-<<<<<<< HEAD
 echo "third email send";
-=======
-echo "feature email send test from feature branch!";
->>>>>>> feature_email
