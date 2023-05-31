663abed0131559c6798630b2ef467d809ab78107
tin htoo aung <tinhtooaung@tinnoMacBook-Pro.local>
2023-05-31 12:47:44 +0900

    conflic solve

--- email.php
+++ email.php
@@ -5,4 +5,8 @@ echo "email sending programm";
 
 echo "second email send";
 
+<<<<<<< HEAD
 echo "third email send";
+=======
+echo "feature email send test from feature branch!";
+>>>>>>> feature_email
